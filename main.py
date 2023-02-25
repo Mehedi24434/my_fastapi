c3edffc99c622895ef643a49dc43fb084a12e18f
Mehedi24434 <mehedi24434@gmail.com>
2023-02-25 12:00:05 +0600

    25 feb update

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from typing import Union
+from typing import Optional
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hi I am Mehedi"}
-@app.get("/about")
-def about():
-    return {'data':{'name':'Mehedi'}}
+@app.get("/blog")
+def blog(limit, published:bool=True):
+    if published:
+        return {'data':f'{limit} published blogs in list of limit'}
+    else:
+        return {'data': f'{limit} blogs in list of limit'}
+
+@app.get("/blog/{id}")
+
+def show(id:int):
+
+    # fetch blog with id = id
+    return {'data':id}
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # fetch blog with comments id = id
+    return {'data':{'comments':id+" num comment"}}
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]              
+@app.post('/blog')
+
+def create_blog(blog:Blog):
+    return {'data':f'the Blog is created as per {blog.title}'}    
+
